238c54f9529d89fd4c1a0832c5c83b2210f21e2f
daef <daef@sourcemonk.com>
2023-08-26 23:44:32 +0200

    uniqueify daefxy-uuid

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -120,7 +120,7 @@
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "4ecb5f6d-daef-436f-99d7-0f853bfbe594"
 
 // @section stepper drivers
 
