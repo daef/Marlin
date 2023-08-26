351ef9f58d47d2be11dc654bdf10ef8460a74df9
daef <daef@sourcemonk.com>
2023-08-26 23:44:48 +0200

    do some basic pid tunin

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -679,9 +679,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  21.5
+    #define DEFAULT_Ki   1.5
+    #define DEFAULT_Kd  75
   #endif
 #else
   #define BANG_MAX 255    // Limit hotend current while in bang-bang mode; 255=full current
