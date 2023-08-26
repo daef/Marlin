258040e99b3044e9211bae6bf65b90862f3b6138
daef <daef@sourcemonk.com>
2023-08-27 00:18:39 +0200

    better offsets

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1776,8 +1776,8 @@
 #define Y_BED_SIZE 310
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -10
-#define Y_MIN_POS -25
+#define X_MIN_POS -2
+#define Y_MIN_POS -20
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
